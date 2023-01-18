9a9c9631f236cee9e738c3841f73bf628de3a0c8
Alain Tresor <tresoralain35@gmail.com>
2023-01-18 10:55:38 +0200

    touch on detail page

--- src/pages/search.js
+++ src/pages/search.js
@@ -6,71 +6,66 @@ import Footer from "../components/Sections/Footer";
 import HotelSearchCard from "../components/HotelSearchCard";
 
 export default function Search() {
-    
-    let { place, arrival,deperture,room,adult,child } = useParams()
-    const [rooms,setRooms] = useState([])
-    const [hotels,setHotel] = useState([])
-    var data=JSON.stringify({
-        "location":place,
-        "children":child,
-        "adults":adult,
-        "roomsNber":room
-    });
-    var config = {
-        method: 'post',
-        url: 'https://bookme.up.railway.app/api/v1/search',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        data : data
-      };
-    useEffect(() => {
-axios(config)
-.then( (res)=> {
-    res.data.result.map((element)=>{
-        axios
-        .get(`https://bookme.up.railway.app/api/v1/accomodation/${element.accomodationId}`)
-        .then((res) => {
-            setHotel([...hotels,res.data.data])
-            // const { accomodation } = res.data.data
-            console.log(res.data.data)
-            // setHotel(accomodation)
-        })
-        .catch((err) => {
-        });
 
-    // console.log(element.accomodationId)
-    })
-    setRooms(res.data.result)
-   
-})
-.catch(function (error) {
-  console.log(error);
-});
-    }, [])
+  let { place, arrival, deperture, room, adult, child } = useParams()
+  const [rooms, setRooms] = useState([])
+  const [hotels, setHotel] = useState([])
+  var data = JSON.stringify({
+    "location": place,
+    "children": child,
+    "adults": adult,
+    "roomsNber": room
+  });
+  var config = {
+    method: 'post',
+    url: 'https://bookme.up.railway.app/api/v1/search',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: data
+  };
+  useEffect(() => {
+    axios(config)
+      .then((res) => {
+        res.data.result.map((element) => {
+          axios
+            .get(`https://bookme.up.railway.app/api/v1/accomodation/${element.accomodationId}`)
+            .then((res) => {
+              setHotel([...hotels, res.data.data])
+              // const { accomodation } = res.data.data
+              console.log(res.data.data)
+              // setHotel(accomodation)
+            })
+            .catch((err) => {
+            });
+
+          // console.log(element.accomodationId)
+        })
+        setRooms(res.data.result)
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [])
   return (
     <>
       <TopNavbar />
-      <div className="SearchContainer">
-       <div className="searchLeft">
-     
-       </div>
-       <div className="searchRight">
-       <div className="">
-        {hotels && hotels.map((item)=>(
-   
-   <HotelSearchCard hotel={item}/>
-        )
-          
-        )}
-      
-     
+      <div className="SearchContainer" >
+        <div className="searchLeft">
+
         </div>
-</div>
-     
-    
-       </div>
-     <Footer />
+        <div className="searchRight">
+          <div className="">
+            {hotels && hotels.map((item) => (
+
+              <HotelSearchCard hotel={item} />
+            )
+            )}
+          </div>
+        </div>
+      </div>
+      <Footer />
 
     </>
   );
