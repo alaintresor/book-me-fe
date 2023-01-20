80645a62f31d0b964da4aa2867741288c5c1b786
teerenzo <renzahoemmanuel8@gmail.com>
2023-01-20 14:31:07 +0200

    payment init

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -152,21 +152,7 @@ function CheckOut() {
   const pay = () => {
     Checkout.showLightbox()
   }
-  function generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();//Timestamp
-    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xx-xxxx-4xxx-yxxx-xxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16;//random number between 0 and 16
-      if (d > 0) {//Use timestamp until depleted
-        r = (d + r) % 16 | 0;
-        d = Math.floor(d / 16);
-      } else {//Use microseconds since page-load if supported
-        r = (d2 + r) % 16 | 0;
-        d2 = Math.floor(d2 / 16);
-      }
-      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-  }
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -204,7 +190,9 @@ function CheckOut() {
       })
       .then((res) => {
         const amount = roomMain?.room?.roomCost * rooms * dayNumber;
-        console.log(amount)
+        localStorage.setItem('book',res.data)
+        // console.log(res)
+        // console.log(res)
         var data1 = JSON.stringify({
           "amount": `${amount}`
         });
